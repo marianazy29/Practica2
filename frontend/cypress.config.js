4b988dec1b9abb35dab04b208c6a5eecc49eac30
Mariana Zúñiga <mariana.zuniga@ucb.edu.bo>
2026-02-22 00:34:21 -0400

    Modify cypress.config in frontend

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      console.log(on);
+      console.log(config);
     },
   },
 });
